d70c6488c37f2d3f4e203857d395c6a80d642020
Bryan Oltman <bryanoltman@gmail.com>
2021-12-05 17:54:29 -0500

    Implement heatmap generation

--- 05/05.dart
+++ 05/05.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'dart:math';
+
 class Coordinate {
   late int x;
   late int y;
@@ -23,6 +25,10 @@ class Line {
 
   Line(this.startCoordinate, this.endCoordinate);
 
+  bool get isHorizontal => startCoordinate.y == endCoordinate.y;
+
+  bool get isVertical => startCoordinate.x == endCoordinate.x;
+
   @override
   bool operator ==(Object other) =>
       other is Line &&
@@ -42,13 +48,61 @@ class VentInput {
   }
 
   static VentInput fromFileLines(List<String> fileLines) {
-    final lines = fileLines.map((line) {
-      final parts = line.split('->');
-      final start = Coordinate.fromString(parts[0]);
-      final end = Coordinate.fromString(parts[1]);
-      return Line(start, end);
-    }).toList();
+    final lines = fileLines
+        .map((line) {
+          final parts = line.split('->');
+          final start = Coordinate.fromString(parts[0]);
+          final end = Coordinate.fromString(parts[1]);
+          return Line(start, end);
+        })
+        .where((element) =>
+            // Only consider horizontal or verical lines
+            element.startCoordinate.x == element.endCoordinate.x ||
+            element.startCoordinate.y == element.endCoordinate.y)
+        .toList();
     return VentInput(lines);
+  }
+}
+
+class VentMap {
+  final VentInput input;
+  final List<List<int>> heatmap;
+
+  VentMap(this.input) : heatmap = VentMap.generateHeatmap(input);
+
+  static List<List<int>> generateHeatmap(VentInput input) {
+    final startCoords = input.lines.map((e) => e.startCoordinate).toList();
+    final endCoords = input.lines.map((e) => e.endCoordinate).toList();
+    final allCoords = startCoords + endCoords;
+    final maxXCoord = allCoords
+        .map((e) => e.x)
+        .reduce((value, element) => max(value, element));
+    final maxYCoord = allCoords
+        .map((e) => e.y)
+        .reduce((value, element) => max(value, element));
+
+    var heatmap =
+        List.generate(maxYCoord + 1, (_) => List.filled(maxXCoord + 1, 0));
+
+    for (final line in input.lines) {
+      if (line.isHorizontal) {
+        final startCoordinate =
+            min(line.startCoordinate.x, line.endCoordinate.x);
+        final endCoordinate = max(line.startCoordinate.x, line.endCoordinate.x);
+        for (var i = startCoordinate; i <= endCoordinate; i++) {
+          heatmap[line.startCoordinate.y][i]++;
+        }
+      } else if (line.isVertical) {
+        final startCoordinate =
+            min(line.startCoordinate.y, line.endCoordinate.y);
+        final endCoordinate = max(line.startCoordinate.y, line.endCoordinate.y);
+        for (var i = startCoordinate; i <= endCoordinate; i++) {
+          heatmap[i][line.startCoordinate.x]++;
+        }
+      }
+    }
+
+    return heatmap;
   }
 }
 
--- 05/05_test.dart
+++ 05/05_test.dart
@@ -2,8 +2,7 @@ import 'package:test/test.dart';
 import '05.dart';
 
 main() {
-  group(VentInput, () {
-    final stringValue = '''0,9 -> 5,9
+  final inputString = '''0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
 2,2 -> 2,1
@@ -14,10 +13,48 @@ main() {
 0,0 -> 8,8
 5,5 -> 8,2''';
 
+  group(VentInput, () {
     test('deserializes from string', () {
-      final input = VentInput.fromFileLines(stringValue.split('\n'));
+      final input = VentInput.fromFileLines(inputString.split('\n'));
       expect(input.lines.first, Line(Coordinate(0, 9), Coordinate(5, 9)));
-      expect(input.lines.last, Line(Coordinate(5, 5), Coordinate(8, 2)));
+      expect(input.lines.last, Line(Coordinate(3, 4), Coordinate(1, 4)));
+    });
+  });
+
+  group(VentMap, () {
+    // Expected:
+    // .......1..
+    // ..1....1..
+    // ..1....1..
+    // .......1..
+    // .112111211
+    // ..........
+    // ..........
+    // ..........
+    // ..........
+    // 222111....
+
+    late VentInput input;
+    late VentMap map;
+
+    setUp(() {
+      input = VentInput.fromFileLines(inputString.split('\n'));
+      map = VentMap(input);
+    });
+
+    group('heatmap', () {
+      test('parses correctly', () {
+        expect(map.heatmap, hasLength(10));
+        for (final line in map.heatmap) {
+          expect(line, hasLength(10));
+        }
+
+        final fifthLine = [0, 1, 1, 2, 1, 1, 1, 2, 1, 1];
+        expect(map.heatmap[4], fifthLine);
+
+        final lastLine = [2, 2, 2, 1, 1, 1, 0, 0, 0, 0];
+        expect(map.heatmap.last, lastLine);
+      });
     });
   });
 }
