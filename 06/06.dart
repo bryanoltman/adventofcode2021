19b5c8bb989309a2088c5970fa78e0fabbc1db39
Bryan Oltman <bryanoltman@gmail.com>
2021-12-06 13:20:38 -0500

    Solve day 6

--- 06/06.dart
+++ 06/06.dart
@@ -1,34 +1,44 @@
 import 'dart:io';
 
 class LanternfishColony {
-  List<int> fish;
-  LanternfishColony(this.fish);
-
-  List<int> moveToNextDay() {
-    int newFish = 0;
-    for (int i = 0; i < fish.length; i++) {
-      if (fish[i] == 0) {
-        fish[i] = 6;
-        newFish++;
-      } else {
-        fish[i]--;
-      }
+  LanternfishColony(List<int> fish) {
+    for (final f in fish) {
+      fishAtDays[f] = fishAtDays[f]! + 1;
     }
-
-    for (var i = 0; i < newFish; i++) {
-      fish.add(8);
-    }
-
-    return fish;
   }
 
-  List<int> moveAhead({required int days}) {
-    for (var i = 0; i < days; i++) {
-      final currentFish = moveToNextDay();
-      print('day $i: $currentFish');
+  Map<int, int> fishAtDays = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+  };
+
+  void moveToNextDay() {
+    // get the number of new fish we need to make
+    final numFishReproducing = fishAtDays[0]!;
+
+    // Increment existing fish to next day
+    for (int i = 0; i < 8; i++) {
+      fishAtDays[i] = fishAtDays[i + 1]!;
     }
 
-    return fish;
+    // Add new fish
+    fishAtDays[8] = numFishReproducing;
+
+    // Reset reproduced fish
+    fishAtDays[6] = fishAtDays[6]! + numFishReproducing;
+  }
+
+  void moveAhead({required int days}) {
+    for (var i = 0; i < days; i++) {
+      moveToNextDay();
+    }
   }
 }
 
@@ -37,6 +47,10 @@ main() {
   final lines = file.readAsLinesSync();
   final fish = lines[0].split(',').map(int.parse).toList();
   final colony = LanternfishColony(fish);
-  colony.moveAhead(days: 10);
-  print(colony.fish.length);
+  colony.moveAhead(days: 256);
+  int length = 0;
+  for (var i = 0; i < 9; i++) {
+    length += colony.fishAtDays[i]!;
+  }
+  print(length);
 }
--- 06/06_test.dart
+++ 06/06_test.dart
@@ -10,74 +10,13 @@ main() {
       colony = LanternfishColony(initialCounts);
     });
 
-    test('moves to next day', () {
-      colony.moveToNextDay();
-      expect(colony.fish, [2, 3, 2, 0, 1]);
-    });
-
-    test('moves to next day', () {
-      colony.moveToNextDay();
-      expect(colony.fish, [2, 3, 2, 0, 1]);
-    });
-
-    test('moves ahead by multiple days', () {
-      expect(colony.moveAhead(days: 1), [2, 3, 2, 0, 1]);
-      expect(colony.moveAhead(days: 1), [1, 2, 1, 6, 0, 8]);
-      expect(colony.moveAhead(days: 1), [0, 1, 0, 5, 6, 7, 8]);
-      expect(colony.moveAhead(days: 1), [6, 0, 6, 4, 5, 6, 7, 8, 8]);
-      expect(colony.moveAhead(days: 1), [5, 6, 5, 3, 4, 5, 6, 7, 7, 8]);
-      expect(colony.moveAhead(days: 1), [4, 5, 4, 2, 3, 4, 5, 6, 6, 7]);
-      expect(colony.moveAhead(days: 1), [3, 4, 3, 1, 2, 3, 4, 5, 5, 6]);
-      expect(colony.moveAhead(days: 1), [2, 3, 2, 0, 1, 2, 3, 4, 4, 5]);
-      expect(colony.moveAhead(days: 1), [1, 2, 1, 6, 0, 1, 2, 3, 3, 4, 8]);
-      expect(colony.moveAhead(days: 1), [0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 7, 8]);
-      expect(colony.moveAhead(days: 1),
-          [6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 7, 8, 8, 8]);
-      expect(colony.moveAhead(days: 1),
-          [5, 6, 5, 3, 4, 5, 6, 0, 0, 1, 5, 6, 7, 7, 7, 8, 8]);
-      expect(colony.moveAhead(days: 1),
-          [4, 5, 4, 2, 3, 4, 5, 6, 6, 0, 4, 5, 6, 6, 6, 7, 7, 8, 8]);
-      expect(colony.moveAhead(days: 1),
-          [3, 4, 3, 1, 2, 3, 4, 5, 5, 6, 3, 4, 5, 5, 5, 6, 6, 7, 7, 8]);
-      expect(colony.moveAhead(days: 1),
-          [2, 3, 2, 0, 1, 2, 3, 4, 4, 5, 2, 3, 4, 4, 4, 5, 5, 6, 6, 7]);
-      expect(colony.moveAhead(days: 1),
-          [1, 2, 1, 6, 0, 1, 2, 3, 3, 4, 1, 2, 3, 3, 3, 4, 4, 5, 5, 6, 8]);
-      expect(colony.moveAhead(days: 1),
-          [0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 0, 1, 2, 2, 2, 3, 3, 4, 4, 5, 7, 8]);
-      expect(colony.moveAhead(days: 1), [
-        6,
-        0,
-        6,
-        4,
-        5,
-        6,
-        0,
-        1,
-        1,
-        2,
-        6,
-        0,
-        1,
-        1,
-        1,
-        2,
-        2,
-        3,
-        3,
-        4,
-        6,
-        7,
-        8,
-        8,
-        8,
-        8
-      ]);
-    });
-
     test('moves ahead by many more days', () {
       colony.moveAhead(days: 256);
-      expect(colony.fish, hasLength(26984457539));
+      int length = 0;
+      for (var i = 0; i < 9; i++) {
+        length += colony.fishAtDays[i]!;
+      }
+      expect(length, 26984457539);
     });
   });
 }
