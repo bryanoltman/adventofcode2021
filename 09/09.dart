b25a540439a71c04aa9e96a2c507bac37fa66db0
Bryan Oltman <bryanoltman@gmail.com>
2021-12-09 11:03:10 -0500

    Day 9, part 2

--- 09/09.dart
+++ 09/09.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 
+class Coordinate {
+  final int x;
+  final int y;
+
+  Coordinate(this.x, this.y);
+
+  @override
+  operator ==(Object other) {
+    return other is Coordinate && other.x == x && other.y == y;
+  }
+
+  @override
+  int get hashCode {
+    return x.hashCode ^ y.hashCode;
+  }
+}
+
 List<List<int>> parseHeightMap(List<String> lines) => lines
     .where((line) => !line.isEmpty)
     .map((line) => line.split('').map(int.parse).toList())
@@ -37,6 +54,50 @@ List<int> findLowPoints(List<List<int>> heightMap) {
   return lowPoints;
 }
 
+var seenCoordinates = <Coordinate>{};
+
+List<Coordinate> findAdjacentBasinCoords(
+  List<List<int>> heightMap,
+  Coordinate coordinate,
+) {
+  if (seenCoordinates.contains(coordinate)) return [];
+  seenCoordinates.add(coordinate);
+
+  final height = heightMap[coordinate.y][coordinate.x];
+  if (height == 9) return [];
+
+  var coords = [coordinate];
+  if (coordinate.x > 0) {
+    final nextCoord = Coordinate(coordinate.x - 1, coordinate.y);
+    coords.addAll(findAdjacentBasinCoords(heightMap, nextCoord));
+  }
+  if (coordinate.x < heightMap[coordinate.y].length - 1) {
+    final nextCoord = Coordinate(coordinate.x + 1, coordinate.y);
+    coords.addAll(findAdjacentBasinCoords(heightMap, nextCoord));
+  }
+  if (coordinate.y > 0) {
+    final nextCoord = Coordinate(coordinate.x, coordinate.y - 1);
+    coords.addAll(findAdjacentBasinCoords(heightMap, nextCoord));
+  }
+  if (coordinate.y < heightMap.length - 1) {
+    final nextCoord = Coordinate(coordinate.x, coordinate.y + 1);
+    coords.addAll(findAdjacentBasinCoords(heightMap, nextCoord));
+  }
+
+  return coords;
+}
+
+List<List<Coordinate>> findBasins(List<List<int>> heightMap) {
+  var basins = <List<Coordinate>>[];
+  for (int y = 0; y < heightMap.length; y++) {
+    for (int x = 0; x < heightMap[y].length; x++) {
+      basins.add(findAdjacentBasinCoords(heightMap, Coordinate(x, y)));
+    }
+  }
+
+  return basins.where((e) => !e.isEmpty).toList();
+}
+
 main() {
   final lines = File('input.txt').readAsLinesSync();
   final map = parseHeightMap(lines);
@@ -44,4 +105,10 @@ main() {
   final riskLevels = lowPoints.map((e) => e + 1);
   final riskLevelsSum = riskLevels.reduce((value, element) => value + element);
   print(riskLevelsSum);
+
+  final basins = findBasins(map);
+  final basinLengths = basins.map((e) => e.length).toList()
+    ..sort((a, b) => b.compareTo(a));
+  final product = basinLengths[0] * basinLengths[1] * basinLengths[2];
+  print('product is $product');
 }
--- 09/09_test.dart
+++ 09/09_test.dart
@@ -17,6 +17,20 @@ main() {
     heightMap = parseHeightMap(heightMapString.split('\n'));
   });
 
+  test('coordinate equality', () {
+    final coordOne = Coordinate(0, 0);
+    final coordTwo = Coordinate(1, 1);
+    final coordThree = Coordinate(0, 0);
+    expect(coordOne == coordTwo, false);
+    expect(coordOne == coordThree, true);
+
+    final intSet = {1, 2, 1};
+    expect(intSet, hasLength(2));
+
+    final coordSet = {coordOne, coordTwo, coordThree};
+    expect(coordSet, hasLength(2));
+  });
+
   test('finds low points in map', () {
     final lowPoints = findLowPoints(heightMap);
     expect(lowPoints, hasLength(4));
@@ -25,5 +39,15 @@ main() {
     expect(lowPoints, contains(0));
 
     expect(lowPoints.map((e) => e + 1).reduce((v, e) => v + e), 15);
+  });
+
+  test('finds basins', () {
+    final basins = findBasins(heightMap);
+    expect(basins, hasLength(4));
+
+    final basinLengths = basins.map((e) => e.length).toList()
+      ..sort((a, b) => b.compareTo(a));
+    final product = basinLengths[0] * basinLengths[1] * basinLengths[2];
+    expect(product, 1134);
   });
 }
